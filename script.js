508bda0d6ef6ce1dfb6792729c5a345c5ac67934
kenzietzu <116095076+kenzietzu@users.noreply.github.com>
2024-02-16 19:56:59 +0800

    1130216-3

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const position = ['NIO', 'ABNB', 'PYPL', 'GOOG', '', 'DIS', 'QQQ', 'TSLA', ''];
+const position = ['NIO', 'ABNB', 'PYPL', 'GOOG', 'TSLA', 'DIS', 'QQQ', '', ''];
 let price = [];
 let date = [];
 const sumValueText = document.getElementById('sumValue');
