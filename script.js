04ac9b56c040197d750ada173d79f36e6d9a0d82
kenzietzu <116095076+kenzietzu@users.noreply.github.com>
2024-08-02 23:08:55 +0800

    1130802

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const position = ['ENPH', 'U', 'U', 'MCD', 'SBUX', 'TLT', 'INTC', 'ADBE'];
+const position = ['ENPH', 'U', 'U', 'NVDA', 'SNOW', 'TLT', 'INTC', 'ABNB'];
 let price = [];
 let date = [];
 const sumValueText = document.getElementById('sumValue');
