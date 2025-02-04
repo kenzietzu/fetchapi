d56bffb2894b16614c928e90a3c643c9fc68c877
kenzietzu <116095076+kenzietzu@users.noreply.github.com>
2025-02-04 14:52:53 +0800

    1130204

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const position = ['AMD', 'AAPL', 'MSFT', 'TSM', 'ASML', 'TLT', 'INTC', 'ABNB'];
+const position = ['AMD', 'AAPL', 'MSFT', 'TSM', 'ASML', 'TLT', 'INTC', 'NVDA'];
 let price = [];
 let date = [];
 const sumValueText = document.getElementById('sumValue');
